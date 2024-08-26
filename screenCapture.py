df9c1b4f212222373f857293db0a27d88ff47be9
Uzair05 <ubcasim@gmail.com>
2024-08-26 10:44:55 +0800

    disregard small bboxes near right and bottom edge

--- screenCapture.py
+++ screenCapture.py
@@ -2,7 +2,7 @@ import cv2 as cv
 import datetime
 
 
-cap = cv.VideoCapture("rtsp://admin:12345678a@180.188.143.227:580 ! decodebin ! videoconvert ! appsink max-buffers=1 drop=trueqqq")
+cap = cv.VideoCapture("rtsp://admin:hik12345@180.188.143.227:581 ! decodebin ! videoconvert ! appsink max-buffers=1 drop=trueqqq")
 ret, frame = cap.read()
 
 while cap.isOpened():
