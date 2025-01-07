52b9808c4dc2589013fa9ee907f84477e5cd0c02
Uzair05 <ubcasim@gmail.com>
2025-01-07 15:54:39 +0800

    debug

--- apps.py
+++ apps.py
@@ -279,17 +279,17 @@ def main():
 
     p1 = threading.Thread(target=Image_Reader, args=(camera_api_details[camera_choice]["video_link"], collected_images, shutdown))
     p2 = threading.Thread(target=Image_Analysis, args=(collected_images, saving_images, region_to_view_mask, mask, model, shutdown))
-    p3 = threading.Thread(target=Image_Saving, args=(saving_images, sending_images, shutdown))
+    # p3 = threading.Thread(target=Image_Saving, args=(saving_images, sending_images, shutdown))
     # p4 = threading.Thread(target=Image_Sending, args=(sending_images, server_api_details, shutdown))
 
     p1.start()
     p2.start()
-    p3.start()
+    # p3.start()
     # p4.start()
 
     p1.join()
     p2.join()
-    p3.join()
+    # p3.join()
     
 
 
