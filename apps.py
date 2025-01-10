799a00dee97db675811e82e73616f60673411b53
Uzair05 <ubcasim@gmail.com>
2025-01-10 12:21:47 +0800

    Bypass Saving Thread

--- apps.py
+++ apps.py
@@ -79,22 +79,19 @@ def Image_Sending(sending_images_q, api_details, shutdown):
         # time.sleep(1) # might have to adjust
 
 
-def Image_Saving(saving_images_q, sending_images_q, shutdown):
-    while (not shutdown):
-        while (not saving_images_q.empty()):
-            print("Diag: Saving Called")
+def Image_Saving(data_tuple, sending_images_q):
+    print("Diag: Saving Called")
 
-            dtm_, img, himg = saving_images_q.get()
-            fpath = f"./tmp/{dtm_}_i.jpeg"
-            hpath = f"./tmp/{dtm_}_h.jpeg"
-            cv.imwrite(fpath, img)
-            cv.imwrite(hpath, himg)
-            sending_images_q.put((fpath, hpath, dtm_))
+    dtm_, img, himg = data_tuple
+    fpath = f"./tmp/{dtm_}_i.jpeg"
+    hpath = f"./tmp/{dtm_}_h.jpeg"
+    cv.imwrite(fpath, img)
+    cv.imwrite(hpath, himg)
+    sending_images_q.put((fpath, hpath, dtm_))
 
-            del dtm_ # hotfix to cure memory leak issue
-            del img  # hotfix to cure memory leak issue
-            del himg  # hotfix to cure memory leak issue
-        # time.sleep(1) # might have to adjust
+    del dtm_ # hotfix to cure memory leak issue
+    del img  # hotfix to cure memory leak issue
+    del himg  # hotfix to cure memory leak issue
 
 
 def build_human_path_mask(bbox_lists=[], mask=None):
@@ -122,7 +119,7 @@ def analysis_trigger(mask_2d=None, change=10):
     m = m.reshape((-1,))
     return ((np.add.reduce(m)*100)//(m.shape[0])) >= change
 
-def Image_Analysis(collected_images_q, saving_images_q, roi_mask, mask, model, shutdown):
+def Image_Analysis(collected_images_q, sending_images_q, roi_mask, mask, model, shutdown):
 
     bg_subtractor = cv.createBackgroundSubtractorMOG2(varThreshold=50)
     
@@ -176,9 +173,7 @@ def Image_Analysis(collected_images_q, saving_images_q, roi_mask, mask, model, s
                                 m, midx = (m_, idx) if m_ > m else (m, midx)
                             
                             print(f"Diag: Best index {midx}; for {m}")
-                            saving_images_q.put( # send for saving
-                                (dtm_, img[:,:,:], human_images_collection[midx][0])
-                            )
+                            Image_Saving((dtm_, img[:,:,:], human_images_collection[midx][0]), sending_images_q)
                             
                         human_images_collection[:] = [] # empty human collection
                 else:
@@ -274,11 +269,11 @@ def main():
         
     shutdown = False
     collected_images = queue.Queue()
-    saving_images = queue.Queue()
+    # saving_images = queue.Queue()
     sending_images = queue.Queue()
 
     p1 = threading.Thread(target=Image_Reader, args=(camera_api_details[camera_choice]["video_link"], collected_images, shutdown))
-    p2 = threading.Thread(target=Image_Analysis, args=(collected_images, saving_images, region_to_view_mask, mask, model, shutdown))
+    p2 = threading.Thread(target=Image_Analysis, args=(collected_images, sending_images, region_to_view_mask, mask, model, shutdown))
     # p3 = threading.Thread(target=Image_Saving, args=(saving_images, sending_images, shutdown))
     # p4 = threading.Thread(target=Image_Sending, args=(sending_images, server_api_details, shutdown))
 
